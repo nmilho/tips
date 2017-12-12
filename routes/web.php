755b5a9a4ff9f3752ca6fe491983389338b7c4db
Nuno Milho - ULSBA <20344@ulsba.min-saude.pt>
2017-12-12 15:49:58 +0000

    Clean comments in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,11 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 Auth::routes();
-/*Route::get('/home', 'HomeController@index');
-  Route::prefix('admin')->group(function() {
-    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/', 'AdminController@index')->name('admin.dashboard');
-  });*/
 
-  Route::get('/', 'HomeController@index');
-  Route::prefix('admin')->group(function() {
-    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/', 'AdminController@index')->name('admin.dashboard');
-  });
+Route::get('/', 'HomeController@index');
+Route::prefix('admin')->group(function() {
+  Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+  Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+  Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
